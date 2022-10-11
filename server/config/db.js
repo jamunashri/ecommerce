ab1e2bf3891ee1347ccd5a0b0a1fef742c6222bc
jamunashri <jamunashrishanmugasundaram@gmail.com>
2022-10-11 12:58:52 +0530

    Mern stack code of ecommerce

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+try {
+  mongoose.connect("mongodb+srv://domain:domain@cluster0.kczyxyb.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log("Database Connected Successfully");
+} catch (err) {
+  console.log("Database Not Connected");
+}
